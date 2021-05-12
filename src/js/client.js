d90297ae15251dfcb7353c0ee6d29f781662af8b
yukimine <yukimine_hayashi@voyagegroup.com>
2021-05-13 05:31:29 +0900

    ルール調整

--- src/js/client.js
+++ src/js/client.js
@@ -1,5 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 class Layout extends React.Component {
   render() {
@@ -7,5 +7,5 @@ class Layout extends React.Component {
   }
 }
 
-const app = document.getElementById("app");
+const app = document.getElementById('app');
 ReactDOM.render(<Layout />, app);
