5d08cea2e83b9b4cccace4cd0a6c49d8cb73ce67
yukimine <yukimine_hayashi@voyagegroup.com>
2021-05-15 05:44:20 +0900

    state x props

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import Header from './Header';
 import Footer from './Footer';
+import Header from './Header';
 
 export default class Layout extends React.Component {
+  constructor() {
+    super();
+    this.state = { title: 'Welcome' };
+  }
+
   render() {
-    const title = 'Welcome Tsutomu!';
+    setTimeout(() => {
+      this.setState({ title: 'Welcome Tsutomu!' });
+    }, 2000);
     return (
       <div>
-        <Header title={title} />
+        <Header title={this.state.title} />
+        <Header title="Thank you" />
         <Footer />
       </div>
     );
