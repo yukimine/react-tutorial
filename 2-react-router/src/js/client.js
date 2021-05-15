a233ad863b1e92f5a0e7e8d8ca29f68ea1829542
yukimine <yukimine_hayashi@voyagegroup.com>
2021-05-15 23:46:13 +0900

    Routerの設定を追加

--- 2-react-router/src/js/client.js
+++ 2-react-router/src/js/client.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import Archives from './pages/Archives';
+import Featured from './pages/Featured';
+import Settings from './pages/Settings';
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout />, app);
+ReactDOM.render(
+  <Router>
+    <Layout>
+      <Route exact path="/" component={Featured}></Route>
+      <Route exact path="/archives" component={Archives}></Route>
+      <Route exact path="/settings" component={Settings}></Route>
+    </Layout>
+  </Router>
+);
